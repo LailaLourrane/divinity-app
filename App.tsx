9550c7c29e2886710b73bbe1a9fa17b0b52c09a2
Lailiss. <62551089+LailaLourrane@users.noreply.github.com>
2026-03-12 08:37:17 -0300

    Actualizar App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LanguageProvider } from './src/contexts/LanguageContext';
-// 1. Importando as 6 portas (telas) que você acabou de criar!
+
 import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import InventoryScreen from './src/screens/InventoryScreen';
@@ -10,12 +10,11 @@ import FavoritesScreen from './src/screens/FavoritesScreen';
 import CraftingScreen from './src/screens/CraftingScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 
-// 2. Criando o nosso "Corredor" de navegação (Pilha de telas)
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // O Dicionário abraçando tudo!
     <LanguageProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
